4fd7e52133df349d81672c7fa0a3a7c4a605c907
Felipe Roscoche <fe.r@outlook.com>
2017-03-29 22:11:04 -0300

    Added multiple webcams support

--- detectface.py
+++ detectface.py
@@ -3,68 +3,70 @@ import cv2
 import numpy
 import time
 import patternrecognition as pr
+import sys
+
 grayscales=1.5
-cap = cv2.VideoCapture(1)
-#time.sleep(2)
+ttl=20
+totalcameras=2
+#Faces array
 rosto=[]
-#cap.set(4,800)
-#cap.set(5,600)
-#cap.set(6,24)
-#ret,frame = cap.read()
-#ret,frame = cap.read()
-ttl = 3
+cap=[]
+# Find how many cameras are connected
+cameras=0
+for cameras in range(totalcameras):
+    cap.append(cv2.VideoCapture(cameras))
+    print(cap[cameras])
+if cameras is 0:
+    print("No camera found")
+    sys.exit()
+cameras+=1
+print(str(cameras)+" cameras found.\n")
+#Opens cam for bright adjustment
+tts = 1
 timestart = time.time()
-timeend = timestart+ttl
-
+timeend = timestart+tts
 while timeend>time.time():
-    ret,frame = cap.read()
-    
+    for i in range(cameras):
+        ret,frame = cap[i].read()
 
-ttl = 20
+# Defining program time
 timestart = time.time()
 timeend = timestart+ttl
 
+#Starting windows Thread
 cv2.startWindowThread()
-cv2.namedWindow("LiveDetect")
-cv2.imshow("LiveDetect",frame)
-rosto1=frame.copy()
+for i in range(cameras):
+    cv2.namedWindow("Cam "+str(i))
+    ret,frame=cap[i].read()
+    cv2.imshow("Cam "+str(i),frame)
+
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 counter=0
 while timeend>time.time():
-    ret,frame = cap.read()
-    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, grayscales, 5)
-    #print("Found "+str(len(faces))+" face(s)")
-    for (x,y,w,h) in faces:
-        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-        npaux=gray[y:y+h,x:x+w]
-        #rostoaux=cv2.cv.fromarray(gray[x:y,x+w:y+h])
-        rosto.append(npaux)
-        if counter==0:
-            print("Felipe detectado")
-            vetor=pr.calculaLBP(rosto[0],2,2)
-            cv2.putText(frame,"Felipe", (x,y), cv2.FONT_HERSHEY_SIMPLEX, 2, 255)
-        else:
-            vetor2=pr.calculaLBP(rosto[counter],2,2)
-            distancia=pr.distanciaEuclidiana(vetor,vetor2)
-            if distancia<0.1:
+    for i in range(cameras):
+        ret,frame = cap[i].read()
+        gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, grayscales, 5)
+        #print("Found "+str(len(faces))+" face(s)")
+        for (x,y,w,h) in faces:
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+            npaux=gray[y:y+h,x:x+w]
+            #rostoaux=cv2.cv.fromarray(gray[x:y,x+w:y+h])
+            rosto.append(npaux)
+            if counter==0:
+                print("Felipe detectado")
+                vetor=pr.calculaLBP(rosto[0],2,2)
                 cv2.putText(frame,"Felipe", (x,y), cv2.FONT_HERSHEY_SIMPLEX, 2, 255)
             else:
-                cv2.putText(frame,"Nao sei", (x,y), cv2.FONT_HERSHEY_SIMPLEX, 2, 255)
-            print("Distancia: "+str(distancia))
-        cv2.imshow("LiveDetect",frame)
-        counter+=1
-    #rostoaux=cv2.cv.fromarray(gray[x:w,y:h])
-    #cv2.imshow("LiveDetect",rostoaux)
-
-#print(len(rosto))
+                vetor2=pr.calculaLBP(rosto[counter],2,2)
+                distancia=pr.distanciaEuclidiana(vetor,vetor2)
+                if distancia<0.1:
+                    cv2.putText(frame,"Felipe", (x,y), cv2.FONT_HERSHEY_SIMPLEX, 2, 255)
+                else:
+                    cv2.putText(frame,"Nao sei", (x,y), cv2.FONT_HERSHEY_SIMPLEX, 2, 255)
+                print("Distancia: "+str(distancia))
+            cv2.imshow("Cam "+str(i),frame)
+            counter+=1
 cv2.imwrite('result.jpg',npaux)
-#cv2.imshow('rosto1',rosto1)
-#cvmat=cv2.cv.fromarray(frame)
-
-vetor=pr.calculaLBP(npaux,2,2)
-vetor2=pr.calculaLBP(npaux,2,2)
-print(pr.distanciaEuclidiana(vetor,vetor2))
-#cv2.waitKey(0)
 print(len(rosto))
